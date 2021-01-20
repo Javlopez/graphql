cd5255e2fcb5304c75fd81261b22faa417652334
Javier <sjavierlopez@gmail.com>
2021-01-20 08:24:18 -0600

    Working with prisma

--- src/index.js
+++ src/index.js
@@ -1,47 +1,22 @@
 const { ApolloServer } = require('apollo-server');
+const { PrismaClient } = require('@prisma/client');
+const { getUserId } = require('./utils');
+
+const Query = require('./resolvers/Query')
+const Mutation = require('./resolvers/Mutation')
+const User = require('./resolvers/User')
+const Link = require('./resolvers/Link')
 
 const fs = require('fs');
 const path = require('path');
+const prisma = new PrismaClient()
 
 
-let links = [{
-    id: 'link-0',
-    url: 'www.howtographql.com',
-    description: 'Fullstack tutorial for GraphQL'
-}]
-
-// 2
-let idCount = links.length
 const resolvers = {
-    Query: {
-        info: () => `This is the API of a Hackernews Clone`,
-        feed: () => links,
-        link: (parent, args) => {
-            return links.find(link => link.id === args.id);
-        }
-    },
-    Mutation: {
-      post:(parent, args) => {
-          const link = {
-              id: `link-${idCount++}`,
-              description: args.description,
-              url: args.url,
-          }
-          links.push(link)
-          return link
-      },
-        updateLink(parent, args) {
-            const linkIndex = links.findIndex(link => link.id === args.id);
-            if (linkIndex !== -1) {
-                links[linkIndex].url = args.url;
-                links[linkIndex].description = args.description;
-                return links[linkIndex]
-            }
-        },
-        deleteLink(parent, args) {
-            links = links.filter(link => link.id !== args.id);
-        }
-    }
+    Query,
+    Mutation,
+    User,
+    Link
 }
 
 // 3
@@ -50,7 +25,16 @@ const server = new ApolloServer({
         path.join(__dirname, 'schema.graphql'),
         'utf-8'
     ),
-    resolvers
+    resolvers,
+    context: ({req}) => {
+        return {
+            ...req,
+            prisma,
+            userId:
+            req && req.headers.authorization
+            ?getUserId(req) :null
+        }
+    }
 })
 
 
---
+++ src/resolvers/Query.js
@@ -0,0 +1,9 @@
+async function feed(parent, args, context) {
+    return await context.prisma.link.findMany()
+}
+
+module.exports = {
+    feed,
+}
+
+
